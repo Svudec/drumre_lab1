77e3b09616beedf53de4e1b3c04c7a30f1205686
Karlo Sudec <Svudec@users.noreply.github.com>
2021-11-25 23:26:45 +0100

    Initial commit

---
+++ src/main/java/hr/unizg/fer/karlo_sudec/Labos1/Labos1Application.java
@@ -0,0 +1,13 @@
+package hr.unizg.fer.karlo_sudec.Labos1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Labos1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Labos1Application.class, args);
+	}
+
+}
---
+++ src/test/java/hr/unizg/fer/karlo_sudec/Labos1/Labos1ApplicationTests.java
@@ -0,0 +1,13 @@
+package hr.unizg.fer.karlo_sudec.Labos1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Labos1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
